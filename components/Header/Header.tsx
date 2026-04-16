067fadb4883f469f40e883ebdba3ce0db3cc0081
Ivan <ivantieplih@gmail.com>
2026-04-16 19:17:39 +0300

    Home page. header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,19 +1,24 @@
+"use client";
+
 import Link from "next/link";
 import css from "./Header.module.css";
-import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header className={css.header}>
       <div className={css.container}>
         <Link href="/" className={css.logo}>
-          TravelTrucks
+          <span className={css.logoText}>Travel</span>
+          <span className={css.logoText2}>Trucks</span>
         </Link>
         <nav className={css.nav}>
-          <Link href="/" className={css.link}>
+          <Link href="/" className={`${css.link} ${pathname === "/" ? css.active : ""}`}>
             Home
           </Link>
-          <Link href="/catalog" className={css.link}>
+          <Link href="/catalog" className={`${css.link} ${pathname === "/catalog" ? css.active : ""}`}>
             Catalog
           </Link>
         </nav>
